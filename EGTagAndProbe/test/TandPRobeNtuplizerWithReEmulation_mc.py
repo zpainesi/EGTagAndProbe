455b4acfbc9f516f546585c3aa1574ddbde10801
Aravind <aravindsugunan@gmail.com>
2024-02-14 13:37:20 +0100

    Updating the cms-configs

--- EGTagAndProbe/test/TandPRobeNtuplizerWithReEmulation_mc.py
+++ EGTagAndProbe/test/TandPRobeNtuplizerWithReEmulation_mc.py
@@ -95,33 +95,14 @@ if not isMC: # will use 80X
     process.load('EGTagAndProbe.EGTagAndProbe.tagAndProbe_cff')
     process.source = cms.Source("PoolSource",
         fileNames = cms.untracked.vstring(
-            '/store/data/Run2018D/EGamma/MINIAOD/22Jan2019-v2/110000/10A4CCCC-3FB2-7F4B-841C-926A03DFBC61.root'
         ),
 
         secondaryFileNames = cms.untracked.vstring(
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/233/00000/7A7B417B-969F-E811-9CC0-FA163EC80159.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/233/00000/9012D37D-969F-E811-913B-FA163EAF7E70.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/414/00000/004596BD-BBA2-E811-A445-FA163EB5B1F7.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/414/00000/C603AEE4-BBA2-E811-AE3E-FA163EFF1C10.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/4210A02C-83AA-E811-A2FC-FA163E6CD89F.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/18A5473E-82AA-E811-816D-02163E014BB9.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/102E6C7D-7FAA-E811-892E-FA163ED1C998.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/414/00000/F21A0D70-BBA2-E811-A608-FA163EAEFBE1.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/414/00000/E675C0A9-BBA2-E811-810D-FA163E5ACC8E.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/233/00000/A8C98583-969F-E811-B653-FA163E8E54FF.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/50D37695-81AA-E811-8B09-FA163E3116C8.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/5E4E4A7C-7FAA-E811-B494-FA163E8A6FB6.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/F404327D-81AA-E811-B1EF-FA163E818EF8.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/F847F781-7FAA-E811-BF05-FA163EC968BB.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/325/057/00000/66BBB3F2-EB2F-7746-A8FE-3C865C2E652E.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/325/057/00000/4A0AB096-D0D7-524C-A5C1-69A429C5F609.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/233/00000/1C47877C-969F-E811-830E-FA163E346E5B.root',
-	'/store/data/Run2018D/EGamma/RAW/v1/000/321/887/00000/4618ECCB-81AA-E811-8EAF-FA163E22EC2E.root'
 	)
     )
 
 else:
-    process.GlobalTag.globaltag = '123X_mcRun3_2021_realistic_v13'
+    process.GlobalTag.globaltag = '133X_mcRun3_2024_realistic_v8'
     process.load('EGTagAndProbe.EGTagAndProbe.MCanalysis_cff')
     process.source = cms.Source("PoolSource",
      fileNames= cms.untracked.vstring(
@@ -150,7 +131,7 @@ else:
     process = L1TTurnOffUnpackStage2GtGmtAndCalo(process)
 
 
-process.load("L1Trigger.L1TCalorimeter.caloParams_2022_v0_2_cfi")
+process.load("L1Trigger.L1TCalorimeter.caloParams_2023_v0_4_eTRecalib_cfi")
 
 #### handling of cms line options for tier3 submission
 #### the following are dummy defaults, so that one can normally use the config changing file list by hand etc.
