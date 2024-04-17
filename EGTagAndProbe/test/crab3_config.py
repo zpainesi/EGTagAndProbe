bd26fc83a944aa54f91a0b773e277cbe0b3d846f
Haris Painesis <95146846+zpainesi@users.noreply.github.com>
2024-04-17 20:33:44 +0300

    Update crab3_config.py

--- EGTagAndProbe/test/crab3_config.py
+++ EGTagAndProbe/test/crab3_config.py
@@ -4,27 +4,27 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'TandP_2023D_reEmulated_v0'
+config.General.requestName = 'TandP_EG0_2024B_v1_379367-379391'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'TAandPReEmulated_run3.py'
+config.JobType.psetName = 'TandPRobeNtuplizer.py'#'TAandPReEmulated_run3.py'
 config.JobType.allowUndistributedCMSSW = True
 
 config.section_("Data")
-config.Data.inputDataset = '/EGamma0/Run2023D-PromptReco-v2/MINIAOD'
+config.Data.inputDataset = '/EGamma0/Run2024B-PromptReco-v1/MINIAOD'
 ########################################### Parent Dataset #######################################
-config.Data.secondaryInputDataset= '/EGamma0/Run2023D-v1/RAW'
+#config.Data.secondaryInputDataset= '/EGamma0/Run2023D-v1/RAW'
 ##################################################################################################
 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1  #number of events per jobs
 config.Data.totalUnits  = -1  #number of event
-config.Data.outLFNDirBase = '/store/group/dpg_trigger/comm_trigger/L1Trigger/athachay/crabOutputStorage/egLUTDerivation2023'
-config.Data.lumiMask    ='Cert_Collisions2022_355100_362760_Golden.json'
-#config.Data.runRange    ='362616-362616'
+config.Data.outLFNDirBase = '/store/group/dpg_trigger/comm_trigger/L1Trigger/zpainesi/CrabOutputStorage/2024'
+config.Data.lumiMask    ='/afs/cern.ch/work/z/zpainesi/public/Certification/Collisions24/Cert_2024B_DCSonly_170424.txt'
+#config.Data.runRange    ='379367-379391'
 config.Data.publication = False
 config.Data.allowNonValidInputDataset = True
 config.section_("Site")
